200bd70034c598d5351b24079fbb5a15daf3a603
ebarnett57 <ebarnett57@gmail.com>
2018-11-18 19:07:32 -0700

    push

---
+++ ArbolAscendencia/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ArbolAscendencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtGeneraciones = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(496, 438);
+            this.treeView1.TabIndex = 0;
+            // 
+            // txtGeneraciones
+            // 
+            this.txtGeneraciones.Location = new System.Drawing.Point(516, 12);
+            this.txtGeneraciones.Name = "txtGeneraciones";
+            this.txtGeneraciones.Size = new System.Drawing.Size(100, 20);
+            this.txtGeneraciones.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(541, 62);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(541, 109);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtGeneraciones);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtGeneraciones;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ ArbolAscendencia/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArbolAscendencia
+{
+    public partial class Form1 : Form
+    {
+
+        TreeNode treeNode = new TreeNode();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+
+            Generar();
+        }
+
+        private void Generar()
+        {
+            treeView1.Nodes.Add("Edgar Barnett");
+
+
+
+            treeView1.ExpandAll();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+        }
+    }
+}
